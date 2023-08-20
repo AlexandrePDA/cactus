a143b3f8b8fb2ddd4bd6cdf960019080744030e4
AlexandrePDA <a.pires130@gmail.com>
2023-08-20 20:18:37 +0200

    add nav and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
+import { Footer } from "@/Components/Footer";
 import { Faq } from "@/Components/Home/Faq";
 import Hero from "@/Components/Home/Hero";
 import How from "@/Components/Home/How";
 import Together from "@/Components/Home/Together";
 import Whatis from "@/Components/Home/Whatis";
+import NavBar from "@/Components/NavBar";
 
 
 export default function Home() {
   return (
     <div>
+      <NavBar/>
       <Hero />
       <Whatis />
       <How/>
       <Together/>
       <Faq/>
+      <Footer/>
     </div>
   );
 }
