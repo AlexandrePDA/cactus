0b79f062fcf5b8945f3d3316dc3f294412432150
AlexandrePDA <a.pires130@gmail.com>
2023-12-07 14:55:23 +0100

    last commit before test

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -9,7 +9,7 @@ export default async function Onboarding() {
   if (session?.user.askCompetence !== null) redirect("/dashboard");
 
   return (
-    <div className=" p-4 max-w-screen-xl text-dark mx-auto w-full mb-4  flex items-center justify-center flex-col h-screen">
+    <div className=" p-4 max-w-screen-xl text-dark mx-auto w-full mt-12  flex items-center justify-center flex-col mb-64">
       <h1 className="text-center mt-4 text-green font-bold text-2xl">
         Onboarding ✈️
       </h1>
